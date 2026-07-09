71ea3e6fb1327cfe5dc528dff98bdc187c56365f
SandeepKanekal <sandeepkanekal@outlook.com>
2026-07-09 15:09:14 +0530

    Updates for transducer and compliance chamber

--- complete/complete.ino
+++ complete/complete.ino
@@ -1,28 +1,43 @@
 /*
-  This code is based on / adapts logic from the Pulsatile Pump project 
+  This code is based on / adapts logic from the Pulsatile Pump project
   by Herwald SE, Sze DY, Ennis DB, Vezeridis AM.
   Original License: CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
 */
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include "HX711.h"
+#include <Adafruit_ADS1X15.h>
 
 // motor
 const byte motorPin = 9;
-int baseline = 5;
+byte baseline = 5;        // baseline flow rate
+byte pulseRate = 60;      // beats per minute
+byte pulseFraction = 25;  // percentage of the cycle that is systole
 // data from haemod.uk/aortic
 const byte flowVector[100] = {
-  5,54,107,158,201,233,251,255,253,243,230,213,196,178,162,146,132,119,107,95,82,67,47,22,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
+  5, 54, 107, 158, 201, 233, 251, 255, 253, 243, 230, 213, 196, 178, 162, 146, 132, 119, 107, 95, 82, 67, 47, 22, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5
 };
 
-// transducer
-const int pressurePin = A1;
-const double sense = 1.0 / (5.0 * pow(10.0, -6.0) * 5.0 * 1000.0);
-int rawADC = 0;
-float voltage = 0.0;
-float pressure = 0.0;
-float V0 = 0.0;
+// transducer values obtained from experiment. May change depending on transducer model and amplifier.
+const float offsetPressure = 3.7781;               // b
+const float mmHgPerCount = 1.159309e2 * 0.000125f;  // m
+int16_t rawADC;
+float pressure;
 unsigned long startTime;
+
+// compliance chamber air pressure
+const float fluidHeight = 10.0;  // cm
+const float rhog = 0.73556;      // for water, change for other fluids
+const float compOffsetPressure = 5.8301;
+const byte dt = 3;
+const byte sck = 2;
+const float compmmHgPerCount = 3.011542e-5;  // m
+long airPressureRaw;
+float airPressure;
+
+HX711 scale;
+Adafruit_ADS1115 ads;
 
 // push buttons
 const byte pulseButton = 31;
@@ -31,6 +46,7 @@ const byte baseButton = 35;
 const byte confButton = 37;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+byte I2CAddress;
 
 bool buttonPressed(byte pin) {
   if (digitalRead(pin) == LOW) {
@@ -43,7 +59,7 @@ bool buttonPressed(byte pin) {
   return false;
 }
 
-void lcdMessage(const String& msg1, const String& msg2 = "") {
+void lcdMessage(const String &msg1, const String &msg2 = "") {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(msg1);
@@ -51,9 +67,9 @@ void lcdMessage(const String& msg1, const String& msg2 = "") {
   lcd.print(msg2);
 }
 
-// Formula: P = (V - V0) * Sensitivity
-float calcPressure(float inputVoltage) {
-  return (inputVoltage - V0) * sens;
+// Formula logic: P = m*rawADC + b
+float calcPressure(long inputCounts, float mmHgPerCount, float offsetPressure) {
+  return (mmHgPerCount * inputCounts) + offsetPressure;
 }
 
 void setup() {
@@ -61,12 +77,26 @@ void setup() {
   Serial.begin(115200);
   Wire.begin();
 
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1)
+      ;
+  }
+
+  // set up the ADS1115 with AD623
+  ads.setGain(GAIN_ONE);
+  ads.setDataRate(RATE_ADS1115_250SPS);
+
+  // set up the HX711 with the pressure transducer
+  scale.begin(dt, sck);
+  scale.set_gain(64);
+
   delay(1000);
 
   // find lcd address
   for (byte address = 1; address < 127; address++) {
     Wire.beginTransmission(address);
-    if (Wire.endTramission() == 0) {
+    if (Wire.endTransmission() == 0) {
       I2CAddress = address;
       break;
     }
@@ -78,7 +108,7 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Hello World!")
+  lcd.print("Hello World!");
 
   // set up the pins
   pinMode(motorPin, OUTPUT);
@@ -90,7 +120,7 @@ void setup() {
 
   // set parameters
   lcdMessage("Set pulse rate", "BPM = " + String(pulseRate));
-  while (!buttonPressed(conButton)){
+  while (!buttonPressed(confButton)) {
     if (buttonPressed(pulseButton)) {
       if (pulseRate < 120) {
         pulseRate = pulseRate + 5;
@@ -102,7 +132,7 @@ void setup() {
   }
 
   lcdMessage("Set pulse frac.", "PF = " + String(pulseFraction));
-  while (!buttonPressed(conButton)){
+  while (!buttonPressed(confButton)) {
     if (buttonPressed(fracButton)) {
       if (pulseFraction < 120) {
         pulseFraction = pulseFraction + 5;
@@ -114,8 +144,8 @@ void setup() {
   }
 
   lcdMessage("Set baseline", "baseline = " + String(baseline));
-  while (!buttonPressed(conButton)){
-    if (buttonPressed(baseButton)){
+  while (!buttonPressed(confButton)) {
+    if (buttonPressed(baseButton)) {
       if (baseline < 20) {
         baseline = baseline + 5;
       } else {
@@ -124,38 +154,9 @@ void setup() {
       lcdMessage("Set baseline", "baseline = " + String(baseline));
     }
   }
-
-  delay(1000);
-
-  // set V0
-  lcdMessage("Disconnect", "Transducer");
-  delay(2000);
-  lcdMessage("Press conf.", "Button");
-
-  while (!buttonPressed(confButton)) {
-    delay(10);
-  }
-
-  lcdMessage("Setting V0", "Please wait...");
   delay(500);
 
-  int sampleADC = analogRead(pressurePin);
-  V0 = sampleADC * (5.0/1023.0);
-
-  lcdMessage("V0 set to:", String(V0) + " V");
-  delay(1500);
-
-  lcdMessage("Connect", "Transducer");
-  delay(2000);
-  lcdMessage("Press conf.", "Button");
-
-  while (!buttonPressed(confButton)) {
-    delay(10);
-  }
-
-  delay(500);
-
-  lcdMessage("BPM=" + String(pulseRate) + " PF=" + String(pulseFraction), "baseline=" + String(baseline));
+  lcdMessage("BPM=" + String(pulseRate) + " PF=" + String(pulseFraction), "BL=" + String(baseline));
   // turn on the motor
   analogWrite(motorPin, 10);
   delay(1000);
@@ -195,28 +196,45 @@ void loop() {
 
   // motor controlling code
   int bufferLength = 125 - pulseFraction;
-  digitalWrite(13, HIGH);  // turn on the built in led when the heartbeat begins
-  for (int i = 0; i < bufferLength; i++){
-    int currentFlow = flowVector[i];
-    if (currentFlow < baseline) currentFlow = baseline;
+  unsigned long interval_micros = 60000000UL / ((unsigned long)pulseRate * bufferLength);
+  unsigned long nextUpdate = micros();
 
-    analogWrite(motorPin, currentFlow);
+  digitalWrite(13, HIGH);
 
-    if (i > 0) digitalWrite(13, LOW);
+  for (int i = 0; i < bufferLength; i++) {
+    // Wait until the exact scheduled time
+    while ((long)(micros() - nextUpdate) < 0) {
+    }
 
-    unsigned long interval = 60000UL / ((unsigned long)pulseRate * bufferLength);
+    if (i > 0)
+      digitalWrite(13, LOW);
 
+    // Timestamp
     unsigned long currentTime = millis() - startTime;
 
-    // calculate pressure
-    rawADC = analogRead(pressurePin);
-    voltage = rawADC * (5.0 / 1023.0);
-    pressure = calcPressure(voltage); 
+    // Read sensors
+    rawADC = ads.readADC_SingleEnded(0);
+    if (scale.is_ready())
+      airPressureRaw = scale.read();
 
+    // Convert to pressure
+    pressure = calcPressure(rawADC, mmHgPerCount, offsetPressure);
+    airPressure = calcPressure(airPressureRaw, compmmHgPerCount, compOffsetPressure - rhog * fluidHeight);
+
+    // Send data
     Serial.print(currentTime / 1000.0);
     Serial.print(",");
-    Serial.println(pressure);
-     
-    delay(interval);
+    Serial.print(pressure);
+    Serial.print(",");
+    Serial.println(airPressure);
+
+    nextUpdate += interval_micros;
+
+    // Update the motor
+    int currentFlow = flowVector[i];
+    if (currentFlow < baseline)
+      currentFlow = baseline;
+
+    analogWrite(motorPin, currentFlow);
   }
 }
